c0753691900d9ae9367be4945dfd526c310480f3
MalcolmK <malcolm.k.x@gmail.com>
2015-05-07 14:57:45 +0200

    Move UiElementData class to server package.

--- flexiblepower.ui/src/org/flexiblepower/runtime/ui/server/UiElementData.java
+++ flexiblepower.ui/src/org/flexiblepower/runtime/ui/server/UiElementData.java
@@ -2,14 +2,60 @@ package org.flexiblepower.runtime.ui.server;
 
 import java.util.HashMap;
 
-public class UiElementData {
-    private final static HashMap<String, String> data = new HashMap<String, String>();
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    public static void setValue(String key, String value) {
-        data.put(key, value);
+public class UiElementData {
+    public HashMap<String, HashMap<String, String>> data;
+
+    private static UiElementData instance = null;
+    private static final Logger logger = LoggerFactory.getLogger(UiElementData.class);
+
+    public static UiElementData getInstance() {
+        if (instance == null) {
+            instance = new UiElementData();
+        }
+        return instance;
     }
 
-    public static Object getValue(String key) {
-        return data.get(key);
+    public UiElementData() {
+        data = new HashMap<String, HashMap<String, String>>();
+    }
+
+    public static void setValue(String id, String key, String value) {
+
+        logger.info("@UiElementData::setValue: Setting element data.");
+        logger.info("@UiElementData::setValue: With id: " + id);
+        logger.info("@UiElementData::setValue: With key: " + key);
+        logger.info("@UiElementData::setValue: With value: " + value);
+
+        HashMap<String, String> internalData = getInstance().getInternalData(id);
+
+        internalData.put(key, value);
+
+        getInstance().data.put(id, internalData);
+    }
+
+    public static String getValue(String id, String key) {
+        HashMap<String, String> internalData = getInstance().getInternalData(id);
+        if (internalData.containsKey(key)) {
+            return internalData.get(key);
+        } else {
+            return null;
+        }
+    }
+
+    public static HashMap<String, String> getValues(String id) {
+        HashMap<String, String> internalData = getInstance().getInternalData(id);
+        return internalData;
+    }
+
+    private HashMap<String, String> getInternalData(String id) {
+        HashMap<String, String> internalData = data.get(id);
+        if (internalData == null) {
+            internalData = new HashMap<String, String>();
+        }
+
+        return internalData;
     }
 }
